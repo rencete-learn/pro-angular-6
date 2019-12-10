eba1ae961127762b319f1c7b561db840e4d02020
Terence Yu <rencete0@gmail.com>
2019-12-10 23:59:59 +0800

    A: Implement Products table component

--- SportsStore/src/app/admin/product-table/product-table.component.ts
+++ SportsStore/src/app/admin/product-table/product-table.component.ts
@@ -1,15 +1,24 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { Observable, of } from "rxjs";
+
+import { ProductRepositoryService } from 'src/app/model/product-repository.service';
+import { Product } from 'src/app/model/product.model';
 
 @Component({
   selector: 'app-product-table',
   templateUrl: './product-table.component.html',
   styleUrls: ['./product-table.component.css']
 })
-export class ProductTableComponent implements OnInit {
+export class ProductTableComponent {
+  public displayedColumns: string[] = ['id', 'name', 'category', 'price', 'buttons'];
 
-  constructor() { }
+  constructor(private repository: ProductRepositoryService) { }
 
-  ngOnInit() {
+  getProducts(): Observable<Product[]> {
+    return of(this.repository.getProducts());
   }
 
+  deleteProduct(id: number) {
+    this.repository.deleteProduct(id);
+  }
 }
