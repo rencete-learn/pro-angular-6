204f6ab609e3c8b3598f05672f9076db742e0092
Terence Yu <rencete0@gmail.com>
2019-12-11 01:35:00 +0800

    A: Implement Product Editor Component

--- SportsStore/src/app/admin/product-editor/product-editor.component.ts
+++ SportsStore/src/app/admin/product-editor/product-editor.component.ts
@@ -1,15 +1,53 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from "@angular/core";
+import { Router, ActivatedRoute } from "@angular/router";
+
+import { ProductRepositoryService } from "src/app/model/product-repository.service";
+import { Product } from "src/app/model/product.model";
+import { FormGroup, FormControl, Validators } from "@angular/forms";
 
 @Component({
-  selector: 'app-product-editor',
-  templateUrl: './product-editor.component.html',
-  styleUrls: ['./product-editor.component.css']
+  selector: "app-product-editor",
+  templateUrl: "./product-editor.component.html",
+  styleUrls: ["./product-editor.component.css"]
 })
-export class ProductEditorComponent implements OnInit {
+export class ProductEditorComponent {
+  public editing: boolean = false;
+  public product: Product;
 
-  constructor() { }
+  public productForm: FormGroup = new FormGroup(
+    {
+      name: new FormControl('', [Validators.required]),
+      category: new FormControl('', [Validators.required]),
+      description: new FormControl(''),
+      price: new FormControl('', [Validators.required]),
+    }
+  );
 
-  ngOnInit() {
+  constructor(
+    private router: Router,
+    private activeRoute: ActivatedRoute,
+    private repository: ProductRepositoryService
+  ) {
+    this.editing = activeRoute.snapshot.params["mode"] == "edit";
+    this.product = new Product();
+    if (this.editing) {
+      Object.assign(this.product,
+        repository.getProduct(activeRoute.snapshot.params["id"]));
+    }
   }
 
+  saveProduct() {
+    this.repository.saveProduct(this.product);
+    this.router.navigateByUrl("/admin/main/products");
+  }
+
+  getErrorMessage(fc: FormControl): string {
+    let msg = "Enter a proper value for this field"
+    if (fc.hasError('required')) {
+      msg = 'You must enter a value';
+    } else if (fc.hasError('email')) {
+      msg = 'Not a valid email';
+    }
+    return msg;
+  }
 }
